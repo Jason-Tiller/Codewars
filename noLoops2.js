8ba542d18edfe840b6f07cf5d7f158bc84ed389f
Jason Tiller <T.Jason@protonmail.com>
2022-05-17 19:45:33 -0500

    Check if the array contains a value

--- noLoops2.js
+++ noLoops2.js
@@ -4,6 +4,7 @@
 
 // Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
 
+
 function check(a,x){
   
  return a.includes(x)
