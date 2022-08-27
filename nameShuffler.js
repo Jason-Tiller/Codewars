e096b54ebf82ee9935b7bb9f7a1bfb1e9d332f7c
Jason Tiller <T.Jason@protonmail.com>
2022-08-26 20:22:23 -0500

    Swap first and last name

--- nameShuffler.js
+++ nameShuffler.js
@@ -4,14 +4,18 @@
 
 // "john McClane" --> "McClane john"
 
-
 function nameShuffler(str){
+    return str.split(" ").reverse().join(" ")
+    }
+
+
+// function nameShuffler(str){
   
-    let array = str.split(' ') 
-    let first = array[0]
-    let last = array[1]
-    array[0] = last
-    array[1] = first
+//     let array = str.split(' ') 
+//     let first = array[0]
+//     let last = array[1]
+//     array[0] = last
+//     array[1] = first
     
-    return finalString = array.join(' ')
-  }
+//     return finalString = array.join(' ')
+//   }
