763fed66f1168d4b0414ebc1ea9ae3bfe395192e
Jason Tiller <T.Jason@protonmail.com>
2022-08-26 20:22:23 -0500

    Swap first and last name

---
+++ nameShuffler.js
@@ -0,0 +1,17 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+
+function nameShuffler(str){
+  
+    let array = str.split(' ') 
+    let first = array[0]
+    let last = array[1]
+    array[0] = last
+    array[1] = first
+    
+    return finalString = array.join(' ')
+  }
